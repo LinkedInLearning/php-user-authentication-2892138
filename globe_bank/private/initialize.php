634d13a21ee006ba0136247a0f95e18d520e6a04
Kevin Skoglund <kevin@novafabrica.com>
2021-07-30 21:50:40 -0400

    Add assets for 02_04

--- globe_bank/private/initialize.php
+++ globe_bank/private/initialize.php
@@ -26,6 +26,7 @@
   require_once('database.php');
   require_once('query_functions.php');
   require_once('validation_functions.php');
+  require_once('auth_functions.php');
 
   $db = db_connect();
   $errors = [];
---
+++ globe_bank/public/staff/login.php
@@ -0,0 +1,58 @@
+<?php
+require_once('../../private/initialize.php');
+
+$errors = [];
+$username = '';
+$password = '';
+
+if(is_post_request()) {
+
+  $username = $_POST['username'] ?? '';
+  $password = $_POST['password'] ?? '';
+
+  // Validations
+  if(is_blank($username)) {
+    $errors[] = "Username cannot be blank.";
+  }
+  if(is_blank($password)) {
+    $errors[] = "Password cannot be blank.";
+  }
+
+  // if there were no errors, try to login
+  if(empty($errors)) {
+    // Using one variable ensures that msg is the same
+    $login_failure_msg = "Log in was unsuccessful.";
+
+    $admin = find_admin_by_username($username);
+    if($admin) {
+      // try to login
+
+    } else {
+      // no username found
+      $errors[] = $login_failure_msg;
+    }
+  }
+
+}
+
+?>
+
+<?php $page_title = 'Log in'; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
+
+<div id="content">
+  <h1>Log in</h1>
+
+  <?php echo display_errors($errors); ?>
+
+  <form action="login.php" method="post">
+    Username:<br />
+    <input type="text" name="username" value="<?php echo h($username); ?>" /><br />
+    Password:<br />
+    <input type="password" name="password" value="" /><br />
+    <input type="submit" name="submit" value="Submit"  />
+  </form>
+
+</div>
+
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>
