d8e5d2d85dc405c4111e0301106c36a20fecc6c2
Kevin Skoglund <kevin@novafabrica.com>
2021-07-31 14:31:14 -0400

    Finish movie 02_06

--- globe_bank/public/staff/admins/edit.php
+++ globe_bank/public/staff/admins/edit.php
@@ -2,6 +2,8 @@
 
 require_once('../../../private/initialize.php');
 
+require_login();
+
 if(!isset($_GET['id'])) {
   redirect_to(url_for('/staff/admins/index.php'));
 }
--- globe_bank/public/staff/index.php
+++ globe_bank/public/staff/index.php
@@ -1,5 +1,7 @@
 <?php require_once('../../private/initialize.php'); ?>
 
+<?php require_login(); ?>
+
 <?php $page_title = 'Staff Menu'; ?>
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
